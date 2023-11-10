ad1f778e696309fcebe68b461c248c998fe50e96
Jorge Chavez Huincho <djjmygm160399@gmail.com>
2023-11-10 10:14:08 -0500

    iniciando la plantilla del proyecto

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+/*===== CONSTROLADORES =====*/
+
+require_once "controladores/plantilla.controlador.php";
+
+/*===== MODELOS =====*/
+
+
+/*===== INSTANCIA DE PLANTILLA =====*/
+
+$plantilla = new ControladorPlantilla();
+$plantilla->ctrPlantilla();
