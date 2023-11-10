1419101a37b94624d7c3764b8e23fade0031f0e2
Jorge Chavez Huincho <djjmygm160399@gmail.com>
2023-11-10 11:48:44 -0500

    Creando plantilla de inicio de sessionyy registro

---
+++ vistas/modulos/login.php
@@ -0,0 +1,248 @@
+<header>
+    <nav class="navbar navbar-expand-lg navbar-lg navbar-dark bg-dark landing">
+        <div class="container">
+            <a class="navbar-brand" href="#"><img src="https://demo.bootstrapdash.com/dashflat/template/images/logo.svg" alt="lead-ui-kit logo"></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#leadUIMainNav" aria-controls="leadUIMainNav" aria-expanded="false" aria-label="Toggle navigation">
+                <i class="mdi mdi-menu"></i>
+            </button>
+            <div class="collapse navbar-collapse" id="leadUIMainNav">
+                <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Inicio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#quienes_somos">Quienes somos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#proyectos">Proyectos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#contactos">Contactos</a>
+                    </li>
+                </ul>
+                <div class="form-inline">
+                    <button type="button" class="btn btn-outline-light" data-toggle="modal" data-target="#exampleModal-4" data-whatever="@fat">Iniciar Sessión</button>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <div class="landing-sass-header-content bg-dark">
+        <div class="container">
+            <h1 class="text-center font-weight-bold pt-5 pb-30px mb-0 text-white">Proyectos de calidad <br>Cambiando vidas</h1>
+            <div class="text-center mb-5 "><button class="btn btn-light mr-3">Get started</button><button class="btn btn-outline-light">Demo video</button></div>
+        </div>
+    </div>
+    <div class="text-center landing-sass-header-img-wrapper"><img src="vistas/estilos/images/dash/dash_3.png" alt="header-img" class="img-fluid" width="707px"></div>
+</header>
+
+<main>
+    <div class="container">
+        <!-- ============ QUIENES SOMOS =============== -->
+        <section class="mb-5" id="quienes_somos">
+            <div class="row">
+                <div class="col-lg-6">
+                    <h1 class="mb-3">¿Quienes somos?</h1>
+                    <p class="text-muted">En P House E.I.R.L. Construcciones, nos dedicamos a materializar sueños y construir futuros sólidos. Desde [año de inicio], hemos sido líderes en la industria de la construcción, llevando a cabo proyectos que van más allá de estructuras; creamos espacios que inspiran y perduran.</p>
+                    <p class="text-muted">En el mundo de la construcción, somos más que constructores; somos creadores de entornos que inspiran, edificamos hogares que albergan sueños y levantamos estructuras que definen comunidades. Desde nuestra fundación en [año de inicio], hemos liderado el camino en la industria, fusionando la visión innovadora con la artesanía tradicional.</p>
+                </div>
+                <div class="col-lg-6">
+                    <div class="row">
+                        <div class="col-sm-6 mb-5 d-flex align-items-center justify-content-center">
+                            <span class="h1 mb-0 mr-2 text-primary font-weight-bold">320</span>
+                            <span>Proyectos <br> Revenu</span>
+                        </div>
+                        <div class="col-sm-6 mb-5 d-flex align-items-center justify-content-center">
+                            <span class="h1 mb-0 mr-2 text-primary font-weight-bold">100</span>
+                            <span>Trabajadores <br> Satisfaction</span>
+                        </div>
+                        <div class="col-sm-6 mb-5 d-flex align-items-center justify-content-center">
+                            <span class="h1 mb-0 mr-2 text-primary font-weight-bold">199</span>
+                            <span>Clientes <br> Clients</span>
+                        </div>
+                        <div class="col-sm-6 mb-5 d-flex align-items-center justify-content-center">
+                            <span class="h1 mb-0 mr-2 text-primary font-weight-bold">240</span>
+                            <span>Máquinas <br> & Features</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="pb-5 mb-5">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="card border-0 mb-3">
+                            <div class="card-body">
+                                <h3 class="font-weight-bold">Misión</h3>
+                                <p class="text-gray mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum, cupiditate soluta? Animi dolore voluptatem dignissimos vel.</p>
+                                <a href="#!" class="font-weight-bold">READ MORE</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="card border-0 mb-3">
+                            <div class="card-body">
+                                <h3 class="font-weight-bold">Visión</h3>
+                                <p class="text-gray mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum, cupiditate soluta? Animi dolore voluptatem dignissimos vel.</p>
+                                <a href="#!" class="font-weight-bold">READ MORE</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- ================ LISTA DE PROYECTOS ==================== -->
+        <section class="py-5">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-10 mx-auto">
+                        <p class="h1 text-center">Proyectos</p>
+                        <p class="text-gray text-center">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="pb-5 mb-5">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="card border-0 mb-3">
+                            <img src="vistas/estilos/images/dash/dash_1.jpg" alt="blog" class="card-img-top rounded-0">
+                            <div class="card-body">
+                                <span class="text-muted small font-weight-bold d-inline-block mb-2">22 Dec 2020</span>
+                                <h3 class="font-weight-bold">Floating architecture is making waves </h3>
+                                <p class="text-gray mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum, cupiditate soluta? Animi dolore voluptatem dignissimos vel.</p>
+                                <a href="#!" class="font-weight-bold">READ MORE</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="card border-0 mb-3">
+                            <img src="vistas/estilos/images/dash/dash_2.jpg" alt="blog" class="card-img-top rounded-0">
+                            <div class="card-body">
+                                <span class="text-muted small font-weight-bold d-inline-block mb-2">27 Sep 2020</span>
+                                <h3 class="font-weight-bold">Learn design with our new course.</h3>
+                                <p class="text-gray mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum, cupiditate soluta? Animi dolore voluptatem dignissimos vel.</p>
+                                <a href="#!" class="font-weight-bold">READ MORE</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    </div>
+</main>
+
+<div class="container pb-5">
+    <footer class="lead-footer shadow px-5">
+        <div class="row">
+            <div class="col-lg-5 col-md-6">
+                <div class="footer-brand-wrapper"><img src="https://demo.bootstrapdash.com/dashflat/template/images/logo.svg" alt="logo white"></div>
+                <p class="pb-2 mb-4">Premium Support. Join over 40,000 satisfied customers who use Medium Rare
+                    templates.</p>
+            </div>
+            <div class="col-lg-6 ml-md-auto">
+                <div class="row">
+                    <div class="col-sm">
+                        <nav class="nav footer-nav nav-vertical my-5 my-md-0">
+                            <a href="#!" class="nav-link">Inicio</a>
+                            <a href="#!" class="nav-link">Quienes somos</a>
+                            <a href="#!" class="nav-link">Proyectos</a>
+                            <a href="#!" class="nav-link">Contactos</a>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 d-flex flex-column flex-sm-row flex-wrap justify-content-between mt-4">
+                <nav class="lead-social-menu mb-4 mb-sm-0">
+                    <a href="#!"><i class="mdi mdi-twitter-box"></i></a>
+                    <a href="#!"><i class="mdi mdi-facebook-box"></i></a>
+                    <a href="#!"><i class="mdi-linkedin-box"></i></a>
+                    <a href="#!"><i class="mdi mdi-instagram"></i></a>
+                </nav>
+                <p class="footer-text text-gray order-sm-first">©2020 Lead. All rights reserved.</p>
+            </div>
+        </div>
+    </footer>
+</div>
+
+
+
+<!-- ========================
+LOGIN
+======================== -->
+
+
+<div class="col-md-6 col-xl-4 grid-margin stretch-card">
+    <div class="card">
+        <div class="card-body">
+            <div class="modal fade" id="exampleModal-4" tabindex="-1" role="dialog" aria-labelledby="ModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h4 class="modal-title" id="ModalLabel"><b>Iniciar Sessión</b></h4>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <form class="pt-2">
+                                <div class="form-group">
+                                    <label for="Usuario">Usuario</label>
+                                    <input type="email" class="form-control form-control-lg" id="exampleInputEmail1" placeholder="Usuario">
+                                </div>
+                                <div class="form-group">
+                                    <label for="Usuario">Constraseña</label>
+                                    <input type="password" class="form-control form-control-lg" id="exampleInputPassword1" placeholder="Constraseña">
+                                </div>
+                                <div class="mt-3">
+                                    <button type="submit" class="btn btn-block btn-primary btn-lg font-weight-medium auth-form-btn">Ingresar</button>
+                                </div>
+                            </form>
+                        </div>
+                       
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="col-md-6 col-xl-4 grid-margin stretch-card">
+    <div class="card">
+        <div class="card-body">
+            <div class="modal fade" id="exampleModal-4" tabindex="-1" role="dialog" aria-labelledby="ModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h4 class="modal-title" id="ModalLabel"><b>Registrarse</b></h4>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <form class="pt-2">
+                                <div class="form-group">
+                                    <label for="Usuario">Usuario</label>
+                                    <input type="email" class="form-control form-control-lg" id="exampleInputEmail1" placeholder="Usuario">
+                                </div>
+                                <div class="form-group">
+                                    <label for="Usuario">Constraseña</label>
+                                    <input type="password" class="form-control form-control-lg" id="exampleInputPassword1" placeholder="Constraseña">
+                                </div>
+                                <div class="mt-3">
+                                    <button type="submit" class="btn btn-block btn-primary btn-lg font-weight-medium auth-form-btn">Registrar</button>
+                                </div>
+                            </form>
+                        </div>
+                       
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- vistas/plantilla.php
+++ vistas/plantilla.php
@@ -1,1 +1,78 @@
-<h1>Hola</h1>
+<!-- Iniciando session -->
+<?php
+session_start();
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>P HOUSE E.I.R.L.</title>
+    <!-- base:css -->
+    <link rel="stylesheet" href="vistas/estilos/vendors/mdi/css/materialdesignicons.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/MaterialDesign-Webfont/7.3.67/css/materialdesignicons.min.css">
+    
+    <link rel="stylesheet" href="vistas/estilos/vendors/feather/feather.css">
+    <link rel="stylesheet" href="vistas/estilos/vendors/css/vendor.bundle.base.css">
+    <!-- endinject -->
+    <!-- plugin css for this page -->
+    <!-- End plugin css for this page -->
+    <!-- inject:css -->
+    <link rel="stylesheet" href="vistas/estilos/css/horizontal-layout-light/style.css">
+    <!-- endinject -->
+    <link rel="shortcut icon" href="vistas/estilos/images/favicon.png" />
+</head>
+
+<body>
+
+<!-- =================================
+CONTENIDO DEL CUERPO
+================================= -->
+
+    <?php
+    
+    if(isset($_SESSION["iniciarSesion"]) && $_SESSION["iniciarSesion"] == "ok"){
+        echo "Bienvenido al sistema";
+    }else{
+        include "modulos/login.php";
+    }
+
+    ?>
+
+
+    <!-- container-scroller -->
+
+    <!-- base:js -->
+    <script src="vistas/estilos/vendors/js/vendor.bundle.base.js"></script>
+    <!-- endinject -->
+    <!-- Plugin js for this page-->
+    <!-- End plugin js for this page-->
+    <!-- inject:js -->
+    <script src="vistas/estilos/js/off-canvas.js"></script>
+    <script src="vistas/estilos/js/hoverable-collapse.js"></script>
+    <script src="vistas/estilos/js/template.js"></script>
+    <script src="vistas/estilos/js/settings.js"></script>
+    <script src="vistas/estilos/js/todolist.js"></script>
+    <!-- endinject -->
+    <!-- plugin js for this page -->
+    <script src="vistas/estilos/vendors/flot/jquery.flot.js"></script>
+    <script src="vistas/estilos/vendors/flot/jquery.flot.resize.js"></script>
+    <script src="vistas/estilos/vendors/chart.js/Chart.min.js"></script>
+    <script src="vistas/estilos/vendors/progressbar.js/progressbar.min.js"></script>
+    <script src="vistas/estilos/vendors/bootstrap-datepicker/bootstrap-datepicker.min.js"></script>
+    <!-- End plugin js for this page -->
+    <!-- Custom js for this page-->
+    <script src="vistas/estilos/js/dashboard.js"></script>
+    <script src="vistas/estilos/js/todolist.js"></script>
+    <!-- End custom js for this page-->
+</body>
+
+
+<!-- Mirrored from demo.bootstrapdash.com/dashflat/template/demo/horizontal-default-light/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 09 Nov 2023 17:14:22 GMT -->
+
+</html>
